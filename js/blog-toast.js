fd14ca1d6663638e5486a13688ec89f18d366b02
lennytrea <choge.lenny@strathmore.edu>
2026-06-23 19:04:35 +0300

    added a banner and articles to the blog.html

---
+++ js/blog-toast.js
@@ -0,0 +1,19 @@
+// Select all button nodes
+document.querySelectorAll(".db-trigger-link").forEach((button) => {
+  button.addEventListener("click", function () {
+    // Pull the custom data attribute out of the targeted element
+    const articleTitle = this.getAttribute("data-article");
+
+    // Inject the data attribute into our Toast warning text
+    document.getElementById("targetArticleName").textContent =
+      `"${articleTitle}"`;
+
+    //Bootstrap Toast node
+    const toastElement = document.getElementById("dbErrorToast");
+    const bootstrapToast = bootstrap.Toast.getOrCreateInstance(toastElement, {
+      delay: 4500, // Keeps notification visible on screen for 4.5 seconds
+    });
+
+    bootstrapToast.show();
+  });
+});
